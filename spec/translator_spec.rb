f9f5f097caa41e0dc1e9ae9d9144b0a7260cdb84
Justin James <justin.james@yale.edu>
2020-05-09 23:28:57 +0000

    Automatically backed up by Learn

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -1,5 +1,5 @@
 
-describe "load_library" do
+describe "#load_library" do
 let(:emoticons) { ['angel', 'angry', 'bored', 'confused', 'embarrassed', 'fish', 'glasses', 'grinning', 'happy', 'kiss', 'sad', 'surprised', 'wink'] }
 let(:emoticon_symbols) { [:angel, :angry, :bored, :confused, :embarrassed, :fish, :glasses, :grinning, :happy, :kiss, :sad, :surprised, :wink] }
 
