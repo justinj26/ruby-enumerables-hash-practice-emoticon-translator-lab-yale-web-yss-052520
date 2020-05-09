b1ecd68e4537385c2fc8369816612bdd21eb1d1b
Justin James <justin.james@yale.edu>
2020-05-09 23:02:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,36 @@
 # require modules here
+require "yaml"
+eed_file = Rails.root.join('db', 'seeds', 'categories.yml')
+config = YAML::load_file(seed_file)
+Category.create!(config)
 
-def load_library
+def load_library(file_path)
   # code goes here
+  new_hash = {}
+  library = YAML.load_file(file_path)
+   library.map do |(key, value)|
+   new_hash[key] <<  {
+     :english => value[0],
+     :japanse => value[1]
+   }
+   end
+   new_hash
 end
 
-def get_japanese_emoticon
+
+def get_japanese_emoticon(file_path, english_emoticon)
   # code goes here
+  load_library(filepath).select do |(key, value)|
+    value[:english] == english_emoticon
+    return value[:japanese_emoticon]
+ end  
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, japanese_emoticon)
   # code goes here
+  load_library(filepath).select do |(key, value)|
+    value[:japanese] == japanese_emoticon
+   return key
+  end
+  
 end
