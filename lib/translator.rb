e69c908e0961d5b0dee61d67ead245d058db0e2b
Justin James <justin.james@yale.edu>
2020-05-10 00:03:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,6 +23,7 @@ def get_japanese_emoticon(file_path, english_emoticon)
     if value[:english] == english_emoticon
     return value[:japanese_emoticon]
     end
+    "Sorry, that emoticon was not found"
   end  
   
 end
@@ -35,5 +36,5 @@ def get_english_meaning(file_path, japanese_emoticon)
     return key
     end
   end
-  
+  p "Sorry, that emoticon was not found"
 end
